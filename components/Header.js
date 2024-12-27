123974e9db78315521f44b04241fb28efad3fce6
Manish Nemade <166635137+Manishnemade12@users.noreply.github.com>
2024-12-27 20:51:13 +0530

    Update Header.js

--- components/Header.js
+++ components/Header.js
@@ -157,9 +157,7 @@ export default function Header() {
                             </li>
 
                         </ul>
-                        //  <div className="adjustment">
-                        // <p>Copyright &copy; 2024 | Manish_Nemade</p>
-                        // </div> *
+                    
                        <p>.</p>
                     </div>
                 </nav>
